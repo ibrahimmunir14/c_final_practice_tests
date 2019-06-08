4f43e5e1468e76d999634868ffc05f4cc39d5278
Ibrahim Munir-Zubair <im318@ic.ac.uk>
2019-06-08 23:15:27 +0100

    Completed ann Part I Questions 1-5.

--- ann/ann/layer.c
+++ ann/ann/layer.c
@@ -4,7 +4,7 @@
 double sigmoid(double x)
 {
   /**** PART 1 - QUESTION 1 ****/
-  return 42.0; // delete after implementing
+  return 1 / (1 + exp(-x));
   /* 1 MARK */
 }
 
@@ -17,7 +17,16 @@ double sigmoidprime(double x)
 layer_t *layer_create()
 {
   /**** PART 1 - QUESTION 2 ****/
-  return NULL; // delete after implementing
+  layer_t *newLayer = (layer_t *) malloc(sizeof(layer_t));
+  if (!newLayer) return NULL;
+  newLayer->num_inputs = 0;
+  newLayer->num_outputs = 0;
+  newLayer->outputs = NULL;
+  newLayer->prev = NULL;
+  newLayer->next = NULL;
+  newLayer->weights = NULL;
+  newLayer->biases = NULL;
+  newLayer->deltas = NULL;
   /* 2 MARKS */
 }
 
@@ -25,7 +34,28 @@ layer_t *layer_create()
 bool layer_init(layer_t *layer, int num_outputs, layer_t *prev)
 {
   /**** PART 1 - QUESTION 3 ****/
-  return true; // delete after implementing
+  layer->num_outputs = num_outputs;
+  layer->outputs = (double *) calloc(num_outputs, sizeof(double));
+  if (!layer->outputs) return true;
+  if (prev != NULL) {
+      layer->prev = prev;
+      layer->prev->next = layer;
+      layer->num_inputs = layer->prev->num_outputs;
+      layer->biases = (double *) calloc(layer->num_outputs, sizeof(double));
+      if (!layer->biases) return true;
+      layer->deltas = (double *) calloc(layer->num_outputs, sizeof(double));
+      if (!layer->deltas) return true;
+      layer->weights = (double **) calloc(layer->num_outputs, sizeof(double *));
+      if (!layer->weights) return true;
+      for (int i = 0; i < layer->num_inputs; i++) {
+          layer->weights[i] = (double *) malloc(layer->num_outputs * sizeof(double));
+          if (!layer->weights[i]) return true;
+          for (int j = 0; j < layer->num_outputs; j++) {
+              layer->weights[i][j] = ANN_RANDOM();
+          }
+      }
+  }
+  return false;
   /* 4 MARKS */
 }
 
@@ -33,7 +63,16 @@ bool layer_init(layer_t *layer, int num_outputs, layer_t *prev)
 void layer_free(layer_t *layer)
 {
   /**** PART 1 - QUESTION 4 ****/
-
+  free(layer->outputs);
+  free(layer->prev);
+  free(layer->next);
+  free(layer->biases);
+  free(layer->deltas);
+  for (int i = 0; i < layer->num_inputs; i++) {
+      free(layer->weights[i]);
+  }
+  free(layer->weights);
+  free(layer);
   /* 2 MARKS */
 }
 
@@ -42,7 +81,13 @@ void layer_compute_outputs(layer_t const *layer)
 {
   /**** PART 1 - QUESTION 5 ****/
   /* objective: compute layer->outputs */
-
+  for (int j = 0; j < layer->num_outputs; j++) {
+      double weightedSum = 0.0;
+      for (int i = 0; i < layer->num_inputs; i++) {
+          weightedSum += layer->weights[i][j] * layer->prev->outputs[i];
+      }
+      layer->outputs[j] = sigmoid(layer->biases[j] + weightedSum);
+  }
   /* 3 MARKS */
 }
 
