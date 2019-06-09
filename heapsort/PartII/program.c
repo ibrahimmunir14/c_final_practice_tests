c42e13f1e21e964fd484b046fc5dc593303bfbec
Ibrahim Munir-Zubair <im318@ic.ac.uk>
2019-06-09 16:14:38 +0100

    [heapsort complete] Completed Part II.

--- heapsort/PartII/program.c
+++ heapsort/PartII/program.c
@@ -4,59 +4,69 @@
 
 
 char *derived_lookup_table(char *);
-int contains(char *, char );
+
+int contains(char *, char);
 
 int main(void) {
-  
+
     char *s = "attack";
-  
+
     printf("The initial word is: %s\n", s);
-  
+
     char *table = derived_lookup_table(s);
-  
+
     printf("Derived lookup table: %s\n\n", table);
-    
+
+    // must free the table since it was allocated memory in heap
+    free(table);
+
     return EXIT_SUCCESS;
 }
 
 char *derived_lookup_table(char *s) {
 
-	char * o_out = malloc(sizeof(s));
-	
-        if (o_out == NULL) {
-	    perror("malloc");
-	    exit(EXIT_FAILURE);
-	}
+    // malloc the correct size : sizeof gives pointer size
+    //char *o_out = malloc(sizeof(s));
+    char *o_out = (char *) malloc(strlen(s));
 
-	char * o_end = malloc(sizeof(s));
-          
-        o_end = o_out;
+    if (o_out == NULL) {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
-        for ( ; *s != '\0' ; ++s) {
-	    if (!contains(o_out, *s)) {
-	      *o_end++ = *s;
-	    }
-	}
-   
-   	char *out = realloc(out, o_end - o_out + 1);
+    // o_end is a pointer, no need for malloc
+    //char *o_end = malloc(sizeof(s));
+    char *o_end = o_out;
 
-	if (out == NULL) {
-	    perror("realloc");
-	    exit(EXIT_FAILURE);
-	  }
+    for (; *s != '\0'; ++s) {
+        if (!contains(o_out, *s)) {
+            *o_end++ = *s;
+        }
+    }
+    // terminate with a null character
+    *o_end = '\0';
 
-	return out;
+    // realloc needs pointer to block to be resized
+    //char *out = realloc(out, o_end - o_out + 1);
+    char *out = realloc(o_out, o_end - o_out + 1);
+
+    if (out == NULL) {
+        perror("realloc");
+        exit(EXIT_FAILURE);
+    }
+
+    return out;
 
 }
 
 
 int contains(char *s, char c) {
-    
-    for (int i=0; i< strlen(s); i++){
-        if(s[i] == c){
-	   return 1;
-	}
+
+    for (int i = 0; i < strlen(s); i++) {
+        if (s[i] == c) {
+            return 1;
+        }
     }
-    
+
     return 0;
 }
